705c8c64405560ac9b50d0f40f5248e074dd48e4
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-03-30 05:54:57 +0000

    Concat Edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split(); str = words[5] + " " + words[6] + " " + words[12] + " " + words[0]
-print(str)
+words = str.split()
+print(words[5] + words[6] + words[12] + words[0])
