00562eae0f3a32a5f6c8649b233c80bd2938c35e
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-02 06:50:42 +0000

    yas

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 
-def uppercase(c):
-    if ord('A') <= ord(c) <= ord('Z'):
-        return True
-    return False
+def uppercase(str):
+    result = ""
+    for c in str:
+        if ord('a') <= ord(c) <= ord('z'):
+            result += chr(ord(c) - 32)
+        else:
+            result += c
+    print("{}".format(result))
