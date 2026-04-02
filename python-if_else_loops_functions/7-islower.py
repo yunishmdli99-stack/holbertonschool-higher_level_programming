8fd420379cdabfd270cc96a38344105d61b5387d
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-02 06:40:32 +0000

    hello

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -6,8 +6,7 @@ print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
 print("3 is {}".format("lower" if islower("3") else "upper"))
 print("g is {}".format("lower" if islower("g") else "upper"))
-def islower(string):
-    for c in string:
-        if not (ord('a') <= ord(c) <= ord('z')):
-            return False
-    return True
+def islower(c):
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    return False
