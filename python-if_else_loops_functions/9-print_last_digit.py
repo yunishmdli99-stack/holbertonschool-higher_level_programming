1cbfb35811aa3498a94b1361405f188de0bd7caa
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-02 06:56:40 +0000

    last digit of us

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number >= 0):
+        sark = number%10
+        return sark
+    else:
+        sark = number%10
+        return sark - 10
