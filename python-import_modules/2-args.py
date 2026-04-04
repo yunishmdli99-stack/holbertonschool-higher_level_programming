dbb1a38cba86f670084842e233c68aceb381788e
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-04 08:10:52 +0000

    args

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ is "__name__":
+    argc = len(argv) - 1
+    if argc == 1:
+        print("{} argument:".format(argc))
+    elif argc == 0:
+        print("{} arguments.".format(argc))
+    else:
+        print("{} arguments:".format(argc))
+    i = 1
+    while i < len(argv):
+        print("{}: {}".format(i, argv[i]))
+        i += 1
