998cb2aefa71513b97bbc8e5a5ff3a63d357b511
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-04 08:16:36 +0000

    inifnite

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    result = 0
+    i = 1
+    while i <len(argv):
+        result+= int(argv[i])
+        i = i + 1
+    print(result)
