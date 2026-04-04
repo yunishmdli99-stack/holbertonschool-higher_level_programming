0c407a0e8a4e806a22f41b569c2b705cf5ec4d20
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-04 07:59:50 +0000

    calculation

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
+
 if __main__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
