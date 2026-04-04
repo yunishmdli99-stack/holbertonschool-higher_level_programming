2a50f55f3528dc4f919e7c523792ad13a0bc790f
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-04 07:35:05 +0000

    addpy

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    c = a + b
-    return "{a} + {b} = {c}"
+from add_0 import add
+
+a = 1
+b = 2
+
 if __name__ == "__main__":
-    main()
+    print(f"{a} + {b} = {add(a, b)}")
