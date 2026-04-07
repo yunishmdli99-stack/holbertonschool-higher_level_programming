da0465443bc7ecf65633a2869793879300c0d961
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-07 08:06:00 +0000

    truman

--- 1-element_at.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx <= -1:
-        print("None")
-    elif idx > len(my_list) - 1:
-        print("None")
-    else:
-        print(my_list[idx])
