e0754b1b5e315755f9b43bf6a594236758506018
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-08 08:10:31 +0000

    multiple multipov

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        return None
+    else:
+        print("Length: {len(sentence)} - First character: {sentence[0]}")
