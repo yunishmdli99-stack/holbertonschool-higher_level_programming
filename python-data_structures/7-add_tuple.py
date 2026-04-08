79f74d308709d75784bca7cb1384207004934463
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-08 08:05:03 +0000

    Add Tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a(), tuple_b()):
+def add_tuple(tuple_a=(), tuple_b=()):
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     
