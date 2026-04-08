6bfead08099bb5416e3171f066d380328aea7056
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-08 07:13:48 +0000

    Jerome Powell

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = len(my_list) - 1
-    while i >= 0:
-        return my_list[i]
-        i--
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{}".format(my_list[i]))
