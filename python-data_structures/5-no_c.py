b660b56df91f1f08c4231337df7f73eae1ddfafa
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-08 07:31:13 +0000

    No C

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in range(len(my_string)):
+        if my_string[i] == 'c' or my_string[i] == 'C':
+            pass
+        else:
+            print(my_string[i])
