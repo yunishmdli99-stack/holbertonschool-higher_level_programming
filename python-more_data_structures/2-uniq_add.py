4c6530652b50904e29e8d1f53c6aa7884aa66e59
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-10 18:38:11 +0000

    yaddassizliq

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     new_list = []
     for i in my_list:
         if i not in new_list:
-            new_list.append(x)
+            new_list.append(i)
     return sum(new_list)
