96ad76a407dc16f77bd19d1a85a6aaa17a1a0a40
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-10 19:06:35 +0000

    common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -3,6 +3,6 @@ def common_elements(set_1, set_2):
     c_set = set()
     for i in set_1:
         for j in set_2:
-            if i==j:
+            if i == j:
                 c_set.add(i)
     return c_set
