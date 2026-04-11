d76e662ae23d23ca44623ba7bfe5ec1f6bf26495
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-11 07:53:04 +0000

    10 best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
     m = None
-    best = None
+    best_key = None
     for i in a_dictionary:
-        if m < a_dictionary[i]:
-        m = a_dictionary[i]
-        best = i
-    return best
+        if m is None or a_dictionary[i] > m:
+            m = a_dictionary[i]
+            best_key = i
+    return best_key
