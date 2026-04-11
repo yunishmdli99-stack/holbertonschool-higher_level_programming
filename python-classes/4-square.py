5bd4769229542f2a69d3423975a9a5c1b669ed83
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-11 19:26:37 +0000

    444

---
+++ python-classes/4-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class with getter and setter.
+"""
+
+
+class Square:
+    """
+    Square class represents a geometric square shape.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square with validation.
+
+        Args:
+            value (int): The new size to set.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        Returns the current square area.
+        """
+        return self.__size ** 2
