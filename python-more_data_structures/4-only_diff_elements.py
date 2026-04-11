60e8b719fe8de84096eaa212ab7234af5f23d7ce
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-11 07:07:21 +0000

    different elements

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -3,6 +3,13 @@ def only_diff_elements(set_1, set_2):
     c_set = set()
     for i in set_1:
         for j in set_2:
-            if i != j:
+            if i == j:
                 c_set.add(i)
-    return c_set
+    d_set = set()
+    for i in set_1:
+        if i not in c_set:
+            d_set.add(i)
+    for j in set_2:
+        if j not in c_set:
+            d_set.add(i)
+    return d_set
