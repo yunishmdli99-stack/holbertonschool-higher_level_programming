86e6af2036c9bffb66265ede4828b6ff72361539
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-11 19:24:25 +0000

    333

---
+++ python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    Square class represents a geometric square shape.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square (no type/value verification yet).
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        this function returns area of square
+        """
+        return self.__size ** 2
