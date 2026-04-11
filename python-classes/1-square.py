0cb989b23c59d9b90e5ab5e1ebbc92357e46ce49
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-11 18:59:01 +0000

    square

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Square class represents a geometric square shape."""
+
+
+    def _init_(self, size):
+        """Initializes a new Square instance
+
+        Args:
+            size: The size of the square(no type/value verification yet)."""
+
+    self.__size = size
