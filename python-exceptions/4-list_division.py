edd46047e1cba147da140397d75e191caa54fc46
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-14 06:27:15 +0000

    list division'

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def list_divison(my_list_1, my_list_2, list_length):
+def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         result = 0
