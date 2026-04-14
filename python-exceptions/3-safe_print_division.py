cb9460cb587b6552d308f2031af484aa1267e656
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-14 06:14:24 +0000

    safe print divison

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        c = a/b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {:d}".format(c))
+    return c
