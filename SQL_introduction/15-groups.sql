aacc61e5625eb0d7e82cc2a488c461f7a08a9530
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-16 07:11:52 +0000

    groups

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- this script will list scores with their numbers
 -- with descending format
-SELECT score FROM second_table GROUP BY score AS number ORDER BY DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
