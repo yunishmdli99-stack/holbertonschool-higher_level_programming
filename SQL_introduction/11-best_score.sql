57ac0fccb4f11ffa53b0d837d56549d951c8c4a8
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-16 06:56:12 +0000

    best score

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
---this script will get only the best students
+-- this script will get only the best students
 -- whose points are higher than 10
 SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
