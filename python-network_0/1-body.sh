d69b28b63e97d8fadd230c87c771df0bb2c4be59
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-18 18:55:49 +0000

    body

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays the response body only if status code is 200
-curl -s -o body.txt -w "%{http_code}" -X GET "$1" | grep -q "^200$" && cat body.txt
+curl -s -L -o body.txt -w "%{http_code}" -X GET "$1" | grep -q "^200$" && cat body.txt
