bd738ae937498b6d3543370f34b71504c1f967ef
yunishmdli99-stack <yunishmdli99@gmail.com>
2026-04-23 19:02:59 +0000

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -32,7 +32,7 @@ class SimpleAPIHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(b"Not Found")
+            self.wfile.write(error)
 
 
 if __name__ == "__main__":
